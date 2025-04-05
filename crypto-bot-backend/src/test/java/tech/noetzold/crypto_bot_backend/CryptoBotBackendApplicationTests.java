f1ce48232577f829c6df4acea8ebf55662bff622
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 11:55:27 -0300

    Create CryptoBotBackendApplicationTests.java

---
+++ crypto-bot-backend/src/test/java/tech/noetzold/crypto_bot_backend/CryptoBotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.crypto_bot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
