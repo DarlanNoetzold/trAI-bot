0c6b8acd75c5838f97659e5040c3b8ab1f624088
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 11:56:27 -0300

    Create CryptoBotBackendApplication.java

---
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/CryptoBotBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.crypto_bot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoBotBackendApplication.class, args);
+	}
+
+}
