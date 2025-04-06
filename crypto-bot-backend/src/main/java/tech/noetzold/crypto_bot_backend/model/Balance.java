1adc2660cfa51303209967608d21b82516af7f9f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:51:15 -0300

    Update Balance.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/model/Balance.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/model/Balance.java
@@ -1,4 +1,24 @@
 package tech.noetzold.crypto_bot_backend.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "balances")
+@Data
 public class Balance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String asset;
+
+    private BigDecimal free;
+
+    private BigDecimal locked;
+
+    private Long updateTime;
 }
