9c9ba218c47001011c04e618527da1a19e998cfb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:52:23 -0300

    Update WebClientConfig.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/config/WebClientConfig.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/config/WebClientConfig.java
@@ -1,4 +1,34 @@
 package tech.noetzold.crypto_bot_backend.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@RequiredArgsConstructor
 public class WebClientConfig {
+
+    private final BinanceProperties binanceProperties;
+
+    @Bean
+    public WebClient binanceWebClient() {
+        return WebClient.builder()
+                .baseUrl(binanceProperties.getApiUrl())
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .filter(apiKeyHeaderFilter())
+                .build();
+    }
+
+    private ExchangeFilterFunction apiKeyHeaderFilter() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            ClientRequest modifiedRequest = ClientRequest.from(clientRequest)
+                    .headers(headers -> headers.set("X-MBX-APIKEY", binanceProperties.getApiKey()))
+                    .build();
+            return reactor.core.publisher.Mono.just(modifiedRequest);
+        });
+    }
 }
