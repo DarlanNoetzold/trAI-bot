9a0a8d8ed53633cf4ec6428b04d0e5cb68863768
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:52:09 -0300

    Update CandleDTO.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/dto/CandleDTO.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/dto/CandleDTO.java
@@ -1,4 +1,24 @@
 package tech.noetzold.crypto_bot_backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CandleDTO {
+    private Long openTime;
+    private BigDecimal open;
+    private BigDecimal high;
+    private BigDecimal low;
+    private BigDecimal close;
+    private BigDecimal volume;
+    private Long closeTime;
+    private BigDecimal quoteAssetVolume;
+    private Long numberOfTrades;
+    private BigDecimal takerBuyBaseVolume;
+    private BigDecimal takerBuyQuoteVolume;
 }
