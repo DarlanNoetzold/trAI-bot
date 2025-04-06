59f84dcd9ee5ad70baafb7568bcb99e6369b4325
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:51:42 -0300

    Update StrategyService.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/service/StrategyService.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/service/StrategyService.java
@@ -1,4 +1,46 @@
 package tech.noetzold.crypto_bot_backend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import tech.noetzold.crypto_bot_backend.model.Strategy;
+import tech.noetzold.crypto_bot_backend.repository.StrategyRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class StrategyService {
+
+    private final StrategyRepository strategyRepository;
+
+    public Strategy saveStrategy(Strategy strategy) {
+        return strategyRepository.save(strategy);
+    }
+
+    public List<Strategy> listAllStrategies() {
+        return strategyRepository.findAll();
+    }
+
+    public Optional<Strategy> getStrategyById(Long id) {
+        return strategyRepository.findById(id);
+    }
+
+    public void deleteStrategy(Long id) {
+        strategyRepository.deleteById(id);
+    }
+
+    public Strategy activateStrategy(Long id) {
+        Strategy strategy = strategyRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Estratégia não encontrada"));
+        strategy.setActive(true);
+        return strategyRepository.save(strategy);
+    }
+
+    public Strategy deactivateStrategy(Long id) {
+        Strategy strategy = strategyRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Estratégia não encontrada"));
+        strategy.setActive(false);
+        return strategyRepository.save(strategy);
+    }
 }
