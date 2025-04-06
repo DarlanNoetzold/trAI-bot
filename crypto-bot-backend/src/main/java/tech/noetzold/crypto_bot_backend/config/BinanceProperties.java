1a0922af42bad0737fb46c0faa66e67af06af9db
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:51:03 -0300

    Update BinanceProperties.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/config/BinanceProperties.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/config/BinanceProperties.java
@@ -1,4 +1,18 @@
 package tech.noetzold.crypto_bot_backend.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "binance")
 public class BinanceProperties {
+
+    private String apiUrl;
+    private String streamUrl;
+    private String apiKey;
+    private String secretKey;
+    private String symbol;
+    private double profitability;
 }
