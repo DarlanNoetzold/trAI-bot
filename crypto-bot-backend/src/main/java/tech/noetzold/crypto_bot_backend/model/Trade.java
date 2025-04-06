2db16c0c85a55eabe485a98901abd9aa3a626ac2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:51:20 -0300

    Update Trade.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/model/Trade.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/model/Trade.java
@@ -1,4 +1,35 @@
 package tech.noetzold.crypto_bot_backend.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "trades")
+@Data
 public class Trade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String symbol;
+
+    private BigDecimal price;
+
+    private BigDecimal quantity;
+
+    private BigDecimal quoteQty;
+
+    private Boolean isBuyer;
+
+    private Boolean isMaker;
+
+    private Boolean isBestMatch;
+
+    private Long binanceTradeId;
+
+    private LocalDateTime executedAt;
 }
