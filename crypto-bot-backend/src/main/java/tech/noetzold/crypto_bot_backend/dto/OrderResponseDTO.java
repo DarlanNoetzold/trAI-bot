fb9a3780266e15f118de3f6c6c34abd854f33c13
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:51:09 -0300

    Update OrderResponseDTO.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/dto/OrderResponseDTO.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/dto/OrderResponseDTO.java
@@ -1,4 +1,14 @@
 package tech.noetzold.crypto_bot_backend.dto;
 
+import lombok.Data;
+
+@Data
 public class OrderResponseDTO {
+    private Long orderId;
+    private String status;
+    private String symbol;
+    private String side;
+    private String type;
+    private Double executedQty;
+    private String clientOrderId;
 }
