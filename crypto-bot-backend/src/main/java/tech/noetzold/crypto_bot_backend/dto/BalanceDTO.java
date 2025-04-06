3242798f417823d9708df622a2e650d4b452077d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:52:11 -0300

    Update BalanceDTO.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/dto/BalanceDTO.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/dto/BalanceDTO.java
@@ -1,4 +1,12 @@
 package tech.noetzold.crypto_bot_backend.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class BalanceDTO {
+    private String asset;
+    private BigDecimal free;
+    private BigDecimal locked;
 }
