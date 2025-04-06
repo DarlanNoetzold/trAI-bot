b9d2e62a093f78761abc83dfe108246f216d1df1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:51:30 -0300

    Update StrategyRepository.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/repository/StrategyRepository.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/repository/StrategyRepository.java
@@ -1,4 +1,9 @@
 package tech.noetzold.crypto_bot_backend.repository;
 
-public class StrategyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tech.noetzold.crypto_bot_backend.model.Strategy;
+
+@Repository
+public interface StrategyRepository extends JpaRepository<Strategy, Long> {
 }
