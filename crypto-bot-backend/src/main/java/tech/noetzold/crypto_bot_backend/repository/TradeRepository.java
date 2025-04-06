c786dd29fc57629ffab6ac41e9fb7e09b6bf0c98
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-05 23:51:32 -0300

    Update TradeRepository.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/repository/TradeRepository.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/repository/TradeRepository.java
@@ -1,4 +1,9 @@
 package tech.noetzold.crypto_bot_backend.repository;
 
-public class TradeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tech.noetzold.crypto_bot_backend.model.Trade;
+
+@Repository
+public interface TradeRepository extends JpaRepository<Trade, Long> {
 }
