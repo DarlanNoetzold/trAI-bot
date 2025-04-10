c749c88a613e8a96e1131a7931892f85b8b7c770
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-10 17:26:28 -0300

    Create CorsConfig.java

---
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package tech.noetzold.crypto_bot_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todos os endpoints
+                        .allowedOrigins("http://localhost:3000") // Permite o frontend React
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos os headers
+                        .allowCredentials(true); // Permite envio de cookies/sessões
+            }
+        };
+    }
+}
+
