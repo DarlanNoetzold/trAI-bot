34447002b61f06a85257a61fca831201119ce092
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-11 15:29:15 -0300

    Update BinanceProperties.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/config/BinanceProperties.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/config/BinanceProperties.java
@@ -3,16 +3,35 @@ package tech.noetzold.crypto_bot_backend.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import tech.noetzold.crypto_bot_backend.context.BinanceEnvironmentContext;
+import tech.noetzold.crypto_bot_backend.enums.BinanceEnvironment;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "binance")
 public class BinanceProperties {
 
-    private String apiUrl;
+    private String testnetApiUrl;
+    private String productionApiUrl;
     private String streamUrl;
-    private String apiKey;
-    private String secretKey;
+
+    private String testnetApiKey;
+    private String testnetSecretKey;
+    private String productionApiKey;
+    private String productionSecretKey;
+
     private String symbol;
     private double profitability;
+
+    public String getDynamicApiKey() {
+        return BinanceEnvironmentContext.get() == BinanceEnvironment.PRODUCTION
+                ? productionApiKey
+                : testnetApiKey;
+    }
+
+    public String getDynamicSecretKey() {
+        return BinanceEnvironmentContext.get() == BinanceEnvironment.PRODUCTION
+                ? productionSecretKey
+                : testnetSecretKey;
+    }
 }
