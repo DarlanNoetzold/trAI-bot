80d49f25e8f86afc742e47eb9bad48c4d4e54450
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-12 18:56:21 -0300

    Create WebSocketConfig.java

---
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/websocket/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package tech.noetzold.crypto_bot_backend.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSocketConfig {
+
+    private final StrategyLogWebSocketHandler logWebSocketHandler;
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("/ws/strategy-logs", logWebSocketHandler); // agora sem /**
+
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setUrlMap(map);
+        mapping.setOrder(-1);
+        return mapping;
+    }
+
+    @Bean
+    public WebSocketHandlerAdapter handlerAdapter() {
+        return new WebSocketHandlerAdapter();
+    }
+}
