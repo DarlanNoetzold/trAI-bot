d7ce55d0d2bd1824e71ef401f0c4e7fd6dc936c3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-13 11:09:31 -0300

    Update StrategyExecutionLogRepository.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/repository/StrategyExecutionLogRepository.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/repository/StrategyExecutionLogRepository.java
@@ -7,7 +7,7 @@ import tech.noetzold.crypto_bot_backend.model.StrategyExecutionLog;
 
 public interface StrategyExecutionLogRepository extends JpaRepository<StrategyExecutionLog, Long> {
 
-    Page<StrategyExecutionLog> findByUserId(Long userId, Pageable pageable);
+    Page<StrategyExecutionLog> findByUserIdAndStrategyNameOrderByTimestampDesc(Long userId, String strategyName, Pageable pageable);
+    Page<StrategyExecutionLog> findByUserIdOrderByTimestampDesc(Long userId, Pageable pageable);
 
-    Page<StrategyExecutionLog> findByUserIdAndStrategyName(Long userId, String strategyName, Pageable pageable);
 }
