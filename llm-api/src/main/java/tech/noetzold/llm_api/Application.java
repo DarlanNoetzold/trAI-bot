7e8fc699e15ad8883fff5e825aaf77e6f045e8fd
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-15 18:54:59 -0300

    Create Application.java

---
+++ llm-api/src/main/java/tech/noetzold/llm_api/Application.java
@@ -0,0 +1,27 @@
+package tech.noetzold.llm_api;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ChatClient openAiChatClient(OpenAiChatModel chatModel) {
+		return ChatClient.create(chatModel);
+	}
+
+	@Bean
+	public ChatClient ollamaChatClient(OllamaChatModel chatModel) {
+		return ChatClient.create(chatModel);
+	}
+
+}
