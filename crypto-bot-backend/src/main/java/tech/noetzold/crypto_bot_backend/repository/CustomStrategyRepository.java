2b141f8142a2e380ab5433808977bc7fa94f4ac9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-15 11:05:02 -0300

    Update CustomStrategyRepository.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/repository/CustomStrategyRepository.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/repository/CustomStrategyRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.noetzold.crypto_bot_backend.model.CustomStrategy;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomStrategyRepository extends JpaRepository<CustomStrategy, Long> {
+    Optional<CustomStrategy> findByName(String strategyName);
 }
