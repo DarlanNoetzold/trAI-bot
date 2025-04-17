5084dc3a2eae836650a3a711c8be433f430d5a00
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-16 22:50:27 -0300

    Create FuturesApiApplicationTests.java

---
+++ futures-api/src/test/java/tech/noetzold/futures_api/FuturesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.futures_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
