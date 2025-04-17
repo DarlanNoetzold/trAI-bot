f192e1ebc39031fde605aafca2e6bfd32ece1b29
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-17 16:53:16 -0300

    Update AccountController.java

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/controller/AccountController.java
+++ crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/controller/AccountController.java
@@ -1,6 +1,7 @@
 package tech.noetzold.crypto_bot_backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.crypto_bot_backend.service.BinanceAccountService;
@@ -8,12 +9,14 @@ import tech.noetzold.crypto_bot_backend.service.BinanceAccountService;
 @RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
+@Slf4j
 public class AccountController {
 
     private final BinanceAccountService accountService;
 
     @GetMapping("/info")
     public ResponseEntity<?> getAccountInfo() {
+        log.info("GET /api/account/info called");
         return ResponseEntity.ok(accountService.getAccountInfo());
     }
 
@@ -22,6 +25,7 @@ public class AccountController {
             @RequestParam String symbol,
             @RequestParam(required = false, defaultValue = "50") Integer limit
     ) {
+        log.info("GET /api/account/trades called with symbol={}, limit={}", symbol, limit);
         return ResponseEntity.ok(accountService.getAccountTrades(symbol, limit));
     }
 }
