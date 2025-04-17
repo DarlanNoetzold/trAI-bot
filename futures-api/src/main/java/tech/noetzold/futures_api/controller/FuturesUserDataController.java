039da8174855f87093f7544eb8bbcfaca28634c8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-17 16:25:33 -0300

    Update FuturesUserDataController.java

--- futures-api/src/main/java/tech/noetzold/futures_api/controller/FuturesUserDataController.java
+++ futures-api/src/main/java/tech/noetzold/futures_api/controller/FuturesUserDataController.java
@@ -1,6 +1,8 @@
+// FuturesUserDataController.java
 package tech.noetzold.futures_api.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.futures_api.service.FuturesUserDataService;
@@ -8,23 +10,26 @@ import tech.noetzold.futures_api.service.FuturesUserDataService;
 @RestController
 @RequestMapping("/api/futures/userdata")
 @RequiredArgsConstructor
+@Slf4j
 public class FuturesUserDataController {
 
     private final FuturesUserDataService futuresUserDataService;
 
     @PostMapping("/listenKey")
     public ResponseEntity<?> getListenKey() {
+        log.info("POST /api/futures/userdata/listenKey called");
         return ResponseEntity.ok(futuresUserDataService.createListenKey());
     }
 
     @PutMapping("/listenKey")
     public ResponseEntity<?> keepAliveListenKey(@RequestParam String listenKey) {
+        log.info("PUT /api/futures/userdata/listenKey called with listenKey={}", listenKey);
         return ResponseEntity.ok(futuresUserDataService.keepAliveListenKey(listenKey));
     }
 
     @DeleteMapping("/listenKey")
     public ResponseEntity<?> deleteListenKey(@RequestParam String listenKey) {
+        log.info("DELETE /api/futures/userdata/listenKey called with listenKey={}", listenKey);
         return ResponseEntity.ok(futuresUserDataService.deleteListenKey(listenKey));
     }
 }
-
