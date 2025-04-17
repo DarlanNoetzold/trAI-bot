ac44638aa78bbcdabb1bf2eea8bbb9561267a0ac
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-17 16:25:30 -0300

    Update FuturesTradeController.java

--- futures-api/src/main/java/tech/noetzold/futures_api/controller/FuturesTradeController.java
+++ futures-api/src/main/java/tech/noetzold/futures_api/controller/FuturesTradeController.java
@@ -1,6 +1,7 @@
 package tech.noetzold.futures_api.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.futures_api.dto.FuturesOrderDTO;
@@ -9,18 +10,22 @@ import tech.noetzold.futures_api.service.FuturesTradeService;
 @RestController
 @RequestMapping("/api/futures/trade")
 @RequiredArgsConstructor
+@Slf4j
 public class FuturesTradeController {
 
     private final FuturesTradeService futuresTradeService;
 
     @PostMapping("/order")
     public ResponseEntity<?> createOrder(@RequestBody FuturesOrderDTO orderDTO) {
+        log.info("POST /api/futures/trade/order called with symbol={}, side={}, type={}, quantity={}, price={}, tif={}",
+                orderDTO.getSymbol(), orderDTO.getSide(), orderDTO.getType(),
+                orderDTO.getQuantity(), orderDTO.getPrice(), orderDTO.getTimeInForce());
         return ResponseEntity.ok(futuresTradeService.createOrder(orderDTO));
     }
 
     @DeleteMapping("/order")
     public ResponseEntity<?> cancelOrder(@RequestParam String symbol, @RequestParam Long orderId) {
+        log.info("DELETE /api/futures/trade/order called with symbol={}, orderId={}", symbol, orderId);
         return ResponseEntity.ok(futuresTradeService.cancelOrder(symbol, orderId));
     }
 }
-
