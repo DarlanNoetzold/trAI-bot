6cf377e5b79bde9ba256966fb1577c941c9f7258
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-17 14:44:43 -0300

    Update FuturesTradeController.java

--- futures-api/src/main/java/tech/noetzold/futures_api/controller/FuturesTradeController.java
+++ futures-api/src/main/java/tech/noetzold/futures_api/controller/FuturesTradeController.java
@@ -1,4 +1,26 @@
 package tech.noetzold.futures_api.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.futures_api.dto.FuturesOrderDTO;
+import tech.noetzold.futures_api.service.FuturesTradeService;
+
+@RestController
+@RequestMapping("/api/futures/trade")
+@RequiredArgsConstructor
 public class FuturesTradeController {
+
+    private final FuturesTradeService futuresTradeService;
+
+    @PostMapping("/order")
+    public ResponseEntity<?> createOrder(@RequestBody FuturesOrderDTO orderDTO) {
+        return ResponseEntity.ok(futuresTradeService.createOrder(orderDTO));
+    }
+
+    @DeleteMapping("/order")
+    public ResponseEntity<?> cancelOrder(@RequestParam String symbol, @RequestParam Long orderId) {
+        return ResponseEntity.ok(futuresTradeService.cancelOrder(symbol, orderId));
+    }
 }
+
