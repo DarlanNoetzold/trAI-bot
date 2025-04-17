31080c789cf08c925cd5b8f604e89288300643b1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-17 14:44:47 -0300

    Update FuturesUserDataController.java

--- futures-api/src/main/java/tech/noetzold/futures_api/controller/FuturesUserDataController.java
+++ futures-api/src/main/java/tech/noetzold/futures_api/controller/FuturesUserDataController.java
@@ -1,4 +1,30 @@
 package tech.noetzold.futures_api.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.futures_api.service.FuturesUserDataService;
+
+@RestController
+@RequestMapping("/api/futures/userdata")
+@RequiredArgsConstructor
 public class FuturesUserDataController {
+
+    private final FuturesUserDataService futuresUserDataService;
+
+    @PostMapping("/listenKey")
+    public ResponseEntity<?> getListenKey() {
+        return ResponseEntity.ok(futuresUserDataService.createListenKey());
+    }
+
+    @PutMapping("/listenKey")
+    public ResponseEntity<?> keepAliveListenKey(@RequestParam String listenKey) {
+        return ResponseEntity.ok(futuresUserDataService.keepAliveListenKey(listenKey));
+    }
+
+    @DeleteMapping("/listenKey")
+    public ResponseEntity<?> deleteListenKey(@RequestParam String listenKey) {
+        return ResponseEntity.ok(futuresUserDataService.deleteListenKey(listenKey));
+    }
 }
+
