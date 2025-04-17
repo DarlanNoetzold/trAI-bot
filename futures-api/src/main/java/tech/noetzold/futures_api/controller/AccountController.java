50015c7bd3b03878ca173ac43c63bbc0834c382a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-17 16:25:21 -0300

    Update AccountController.java

--- futures-api/src/main/java/tech/noetzold/futures_api/controller/AccountController.java
+++ futures-api/src/main/java/tech/noetzold/futures_api/controller/AccountController.java
@@ -1,6 +1,7 @@
 package tech.noetzold.futures_api.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,14 @@ import tech.noetzold.futures_api.service.BinanceAccountService;
 @RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
+@Slf4j
 public class AccountController {
 
     private final BinanceAccountService accountService;
 
     @GetMapping("/info")
     public ResponseEntity<?> getAccountInfo() {
+        log.info("GET /api/account/info called");
         return ResponseEntity.ok(accountService.getAccountInfo());
     }
 
@@ -25,6 +28,7 @@ public class AccountController {
             @RequestParam String symbol,
             @RequestParam(required = false, defaultValue = "50") Integer limit
     ) {
+        log.info("GET /api/account/trades called with symbol={}, limit={}", symbol, limit);
         return ResponseEntity.ok(accountService.getAccountTrades(symbol, limit));
     }
 }
