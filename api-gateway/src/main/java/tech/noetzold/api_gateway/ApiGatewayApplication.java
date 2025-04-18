013e7426c6947900a50eda9553d5f97df7e1d26c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-18 11:16:30 -0300

    Create ApiGatewayApplication.java

---
+++ api-gateway/src/main/java/tech/noetzold/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,28 @@
+package tech.noetzold.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("auth-api", r -> r.path("/api/auth/**")
+						.uri("http://localhost:8081"))
+				.route("spot-api", r -> r.path("/api/account/**", "/api/market/**", "/api/trade/**")
+						.uri("http://localhost:8082"))
+				.route("strategy-api", r -> r.path("/api/strategies/**", "/api/custom-strategies/**", "/api/logs/**")
+						.uri("http://localhost:8083"))
+				.route("futures-api", r -> r.path("/api/futures/**")
+						.uri("http://localhost:8084"))
+				.build();
+	}
+}
