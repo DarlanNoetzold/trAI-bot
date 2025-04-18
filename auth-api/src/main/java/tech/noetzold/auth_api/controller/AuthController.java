68515ced56fe45d1ab7c151261f7abbe7394067d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-18 11:16:13 -0300

    Create AuthController.java

---
+++ auth-api/src/main/java/tech/noetzold/auth_api/controller/AuthController.java
@@ -0,0 +1,35 @@
+package tech.noetzold.auth_api.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.auth_api.dto.AuthResponseDTO;
+import tech.noetzold.auth_api.dto.UserLoginDTO;
+import tech.noetzold.auth_api.dto.UserRegisterDTO;
+import tech.noetzold.auth_api.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@Slf4j
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody UserRegisterDTO dto) {
+        log.info("POST /api/auth/register called with email={}, username={}", dto.getEmail(), dto.getUsername());
+        authService.register(dto);
+        return ResponseEntity.ok("Usuário registrado com sucesso");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody UserLoginDTO dto) {
+        log.info("POST /api/auth/login called with email={}", dto.getEmail());
+        return ResponseEntity.ok(authService.login(dto));
+    }
+}
