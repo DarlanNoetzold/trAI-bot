e35feb02dda9aa58f1ef33392dcdfa4aa1eb488d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-18 11:16:11 -0300

    Create AuthResponseDTO.java

---
+++ auth-api/src/main/java/tech/noetzold/auth_api/dto/AuthResponseDTO.java
@@ -0,0 +1,12 @@
+package tech.noetzold.auth_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String token;
+    private String username;
+}
+
