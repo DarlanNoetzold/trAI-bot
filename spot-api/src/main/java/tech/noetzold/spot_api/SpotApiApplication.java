ed0ea99497a8252b096defccd730e2ecee578355
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-18 23:32:17 -0300

    Create SpotApiApplication.java

---
+++ spot-api/src/main/java/tech/noetzold/spot_api/SpotApiApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.spot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotApiApplication.class, args);
+	}
+
+}
