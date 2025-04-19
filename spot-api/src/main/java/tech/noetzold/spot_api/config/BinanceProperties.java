cd3eb2e86e7146436fc5ecc1fce8b9b1f52a64cc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-18 23:33:53 -0300

    Create BinanceProperties.java

---
+++ spot-api/src/main/java/tech/noetzold/spot_api/config/BinanceProperties.java
@@ -0,0 +1,37 @@
+package tech.noetzold.spot_api.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import tech.noetzold.spot_api.context.BinanceEnvironmentContext;
+import tech.noetzold.spot_api.enums.BinanceEnvironment;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "binance")
+public class BinanceProperties {
+
+    private String testnetApiUrl;
+    private String productionApiUrl;
+    private String streamUrl;
+
+    private String testnetApiKey;
+    private String testnetSecretKey;
+    private String productionApiKey;
+    private String productionSecretKey;
+
+    private String symbol;
+    private double profitability;
+
+    public String getDynamicApiKey() {
+        return BinanceEnvironmentContext.get() == BinanceEnvironment.PRODUCTION
+                ? productionApiKey
+                : testnetApiKey;
+    }
+
+    public String getDynamicSecretKey() {
+        return BinanceEnvironmentContext.get() == BinanceEnvironment.PRODUCTION
+                ? productionSecretKey
+                : testnetSecretKey;
+    }
+}
