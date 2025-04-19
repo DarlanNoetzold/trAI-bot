352803559f7ec75ad84ead528d59fb5b4cf49bde
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-18 23:31:35 -0300

    Create SpotApiApplicationTests.java

---
+++ spot-api/src/test/java/tech/noetzold/spot_api/SpotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.spot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
