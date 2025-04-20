8002b37b9a3d316e4299d6d95b7bea2a6b02dcda
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-20 15:32:50 -0300

    Create UserRegisterDTO.java

---
+++ notification-api/src/main/java/tech/noetzold/notification_api/dto/UserRegisterDTO.java
@@ -0,0 +1,21 @@
+package tech.noetzold.notification_api.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class UserRegisterDTO {
+    private String username;
+    private String email;
+    private String password;
+    private LocalDate birthDate;
+    private String address;
+    private String testnetApiKey;
+    private String testnetSecretKey;
+    private String productionApiKey;
+    private String productionSecretKey;
+    private String whatsappNumber;
+    private String telegramChatId;
+}
+
