99f8bc667747c59461db527ab80a851520aa27c1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-20 15:32:55 -0300

    Create User.java

---
+++ notification-api/src/main/java/tech/noetzold/notification_api/model/User.java
@@ -0,0 +1,27 @@
+package tech.noetzold.notification_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String birthDate;
+    private String address;
+
+    private String testnetApiKey;
+    private String testnetSecretKey;
+    private String productionApiKey;
+    private String productionSecretKey;
+    private String whatsappNumber;
+    private String telegramChatId;
+}
