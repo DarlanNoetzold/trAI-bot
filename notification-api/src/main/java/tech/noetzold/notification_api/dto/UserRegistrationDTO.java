f795ffcd20164d8a0e41ef9e893d7d95bf68478e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-20 15:32:53 -0300

    Create UserRegistrationDTO.java

---
+++ notification-api/src/main/java/tech/noetzold/notification_api/dto/UserRegistrationDTO.java
@@ -0,0 +1,20 @@
+package tech.noetzold.notification_api.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class UserRegistrationDTO {
+    private String username;
+    private String email;
+    private String password;
+    private LocalDate birthDate;
+    private String address;
+    private String binanceTestnetApiKey;
+    private String binanceTestnetSecretKey;
+    private String binanceProductionApiKey;
+    private String binanceProductionSecretKey;
+    private String whatsappNumber;
+    private String telegramChatId;
+}
