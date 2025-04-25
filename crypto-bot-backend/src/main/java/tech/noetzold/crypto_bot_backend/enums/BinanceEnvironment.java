ae3352c9965452d4b347a77c1285513d031c29b3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-25 13:40:05 -0300

    .

--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/enums/BinanceEnvironment.java
+++
@@ -1,6 +0,0 @@
-package tech.noetzold.crypto_bot_backend.enums;
-
-public enum BinanceEnvironment {
-    TESTNET,
-    PRODUCTION
-}
--- crypto-bot-backend/src/main/java/tech/noetzold/crypto_bot_backend/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.crypto_bot_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tech.noetzold.crypto_bot_backend.model.Order;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
