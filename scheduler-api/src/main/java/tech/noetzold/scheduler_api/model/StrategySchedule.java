af45d49acffcc6684099407292e3ec25b81b0675
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-05-22 16:16:17 -0300

    Update StrategySchedule.java

--- scheduler-api/src/main/java/tech/noetzold/scheduler_api/model/StrategySchedule.java
+++ scheduler-api/src/main/java/tech/noetzold/scheduler_api/model/StrategySchedule.java
@@ -1,15 +1,11 @@
 package tech.noetzold.scheduler_api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+
 import lombok.Data;
 
 @Data
-@Entity
 public class StrategySchedule {
-    @Id
-    @GeneratedValue
+
     private Long id;
     private Long userId;
     private String strategyName;
