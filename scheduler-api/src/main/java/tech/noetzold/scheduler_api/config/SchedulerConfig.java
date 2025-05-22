142d41e521867c906c881d02e9c5d8becc16e0d1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-05-22 16:16:15 -0300

    Create SchedulerConfig.java

---
+++ scheduler-api/src/main/java/tech/noetzold/scheduler_api/config/SchedulerConfig.java
@@ -0,0 +1,19 @@
+package tech.noetzold.scheduler_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("strategy-scheduler-");
+        scheduler.initialize();
+        return scheduler;
+    }
+}
