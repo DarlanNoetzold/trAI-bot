92f7679d9c5ca434e6f2fd6f2c36c2fe2a04194b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-05-26 09:33:21 -0300

    Update AuditLog.java

--- spot-api/src/main/java/tech/noetzold/spot_api/model/AuditLog.java
+++ spot-api/src/main/java/tech/noetzold/spot_api/model/AuditLog.java
@@ -2,17 +2,21 @@ package tech.noetzold.spot_api.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.time.Instant;
 
 @Entity
+@Data
 public class AuditLog {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long userId;
+    private String email;
     private String action;
     private String resource;
     private String description;
