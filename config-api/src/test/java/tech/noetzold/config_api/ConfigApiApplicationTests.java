2080630d3b8009f4ea403a6f82272f963d2b3321
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-05-27 10:12:56 -0300

    Create ConfigApiApplicationTests.java

---
+++ config-api/src/test/java/tech/noetzold/config_api/ConfigApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.config_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
