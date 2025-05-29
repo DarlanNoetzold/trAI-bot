06f4f9b60d1a785b23815a8643b72fe6e3de1fed
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-05-29 18:30:18 -0300

    Update AuditLog.java

--- auth-api/src/main/java/tech/noetzold/auth_api/model/AuditLog.java
+++ auth-api/src/main/java/tech/noetzold/auth_api/model/AuditLog.java
@@ -1,18 +1,23 @@
 package tech.noetzold.auth_api.model;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.time.Instant;
 
 @Entity
+@Data
 public class AuditLog {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long userId;
+    private String email;
     private String action;
     private String resource;
     private String description;
