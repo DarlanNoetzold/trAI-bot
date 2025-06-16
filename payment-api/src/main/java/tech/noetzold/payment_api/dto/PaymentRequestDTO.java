ba9df449acc914a6ca015a1d77260d5a08efe911
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-06-16 14:51:54 -0300

    Update PaymentRequestDTO.java

--- payment-api/src/main/java/tech/noetzold/payment_api/dto/PaymentRequestDTO.java
+++ payment-api/src/main/java/tech/noetzold/payment_api/dto/PaymentRequestDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class PaymentRequestDTO {
-    private Long amount; // Ex: 999 para $9.99
+    private Long amount;
     private String planName;
     private String successUrl;
     private String cancelUrl;
