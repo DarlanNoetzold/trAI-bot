5644c1e4dc0256c2e09ae7e8929f0f0c34fc5e61
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-06-17 22:16:42 -0300

    Update accountService.js

--- crypto-bot-frontend/src/services/accountService.js
+++ crypto-bot-frontend/src/services/accountService.js
@@ -1,4 +1,3 @@
-// API de conta 
 import api from './api';
 
 export const getAccountInfo = async () => {
@@ -8,5 +7,15 @@ export const getAccountInfo = async () => {
 
 export const getAccountTrades = async (symbol, limit = 50) => {
   const response = await api.get(`/account/trades?symbol=${symbol}&limit=${limit}`);
+  return response.data;
+};
+
+export const getUserInfo = async () => {
+  const response = await api.get('/auth/me');
+  return response.data;
+};
+
+export const updateUserInfo = async (userData) => {
+  const response = await api.put('/auth/update', userData);
   return response.data;
 };
